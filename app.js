4ae50db743b62d6d803d971c287e26e7a8fabfa3
FrankTII <105025372+FrankTII@users.noreply.github.com>
2022-12-13 12:45:28 -0600

    test cors origin localhost

--- app.js
+++ app.js
@@ -16,7 +16,11 @@ app.use("/", routes);
 
 // use helmet and use cors
 app.use(helmet());
-app.use(cors({credentials: true, origin: '*'}));
+
+app.use(cors({
+    origin: 'http://localhost:3000'
+}));
+//app.use(cors({credentials: true, origin: '*'}));
 //app.use(function(req, res, next) {
 //	res.header("Access-Control-Allow-Origin", "http://localhost:3000");
 //	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
